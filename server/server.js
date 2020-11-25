3f13a94f380c324057b63497555dc8e273b5506e
Yeqin Guo <>
2020-11-26 00:45:44 +1100

    added cloud mongo

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ const app = express();
 
 // db
 mongoose
-  .connect(process.env.MONGO_URL, {
+  .connect(process.env.MONGO_CLOUD_URL, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
